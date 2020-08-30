273c4e59ca528110255e5aaa5f27ab034c45b35a
adeliaayu <adeliaayut53@gmail.com>
2020-08-30 23:52:28 +0700

    change background task using coroutine

--- app/src/main/java/com/dicoding/kotlin/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/mybackgroundthread/MainActivity.kt
@@ -5,78 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import java.lang.ref.WeakReference
 
-class MainActivity : AppCompatActivity(), MyAsyncCallback {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val demoAsync = DemoAsync(this)
-        demoAsync.execute(INPUT_STRING)
-    }
-
-    override fun onPreExecute() {
         tv_status.setText(R.string.status_pre)
         tv_desc.text = INPUT_STRING
-    }
 
-    override fun onPostExecute(result: String) {
-        tv_status.setText(R.string.status_post)
-        tv_desc.text = result
+        GlobalScope.launch(Dispatchers.IO) {
+            //background thread
+            val input = INPUT_STRING
+
+            var output: String? = null
+
+            Log.d(LOG_ASYNC, "status: doInBackground")
+            try {
+                //Input stringnya ditambahakan dengan string "Selamat Belajar!!"
+                output = "$input Selamat Belajar!!"
+
+                delay(2000)
+
+                //pindah ke Main thread untuk update UI
+                withContext(Dispatchers.Main) {
+                    tv_status.setText(R.string.status_post)
+                    tv_desc.text = output
+                }
+
+            } catch (e: Exception) {
+                Log.d(LOG_ASYNC, e.message.toString())
+            }
+        }
     }
 
     companion object {
         private const val INPUT_STRING = "Halo ini demo AsyncTask!!"
+        private const val LOG_ASYNC = "DemoAsync"
     }
-
-    private class DemoAsync(myListener: MyAsyncCallback): AsyncTask<String, Void, String>() {
-
-        private val myListener: WeakReference<MyAsyncCallback>
-
-        init {
-            this.myListener = WeakReference(myListener)
-        }
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            Log.d(LOG_ASYNC, "status: onPreExecute")
-
-            val myListener = myListener.get()
-            myListener?.onPreExecute()
-        }
-
-        override fun doInBackground(vararg params: String?): String {
-            Log.d(LOG_ASYNC, "status: doInBackground")
-
-            var output: String? = null
-
-            try {
-                val input = params[0]
-                output = "$input Selamat Belajar!!"
-                Thread.sleep(2000)
-            } catch (e: Exception) {
-                Log.d(LOG_ASYNC, e.message.toString())
-            }
-            return output.toString()
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            Log.d(LOG_ASYNC, "status: onPostExecute")
-
-            val myListener = this.myListener.get()
-            myListener?.onPostExecute(result.toString())
-        }
-
-        companion object {
-            private val LOG_ASYNC = "DemoAsync"
-        }
-
-    }
-}
-
-internal interface MyAsyncCallback {
-    fun onPreExecute()
-    fun onPostExecute(text: String)
 }
